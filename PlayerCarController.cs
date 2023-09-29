4d58153d4f658b19e532f22e4a1adcdfd5dfe0ee
Mudassar Khan <mudassarkhan428@gmail.com>
2023-09-29 17:40:21 +0530

    Added Steering Feature for Player's Car

--- PlayerCarController.cs
+++ PlayerCarController.cs
@@ -21,17 +21,32 @@ public class PlayerCarController : MonoBehaviour
     public float presentAccelerationForce = 0f;
     public float presentBreakingForce = 0f;
 
+    [Header("Car Steering")]
+    private float steeringTorque = 35f;
+    private float presentSteeringTorque = 0f;
+
     private void Update()
     {
         MoveCar();
+        CarSteering();
     }
 
     private void MoveCar()
     {
+        // All Wheel Drive (Power given to All 4 Wheels)
         FrontLeftWheelCollider.motorTorque = presentAccelerationForce;
         FrontRightWheelCollider.motorTorque = presentAccelerationForce;
-
+        BackLeftWheelCollider.motorTorque = presentAccelerationForce;
+        BackRightWheelCollider.motorTorque = presentAccelerationForce;
 
         presentAccelerationForce = AccelerationForce * Input.GetAxis("Vertical");
+    }
+
+    private void CarSteering()
+    {
+        presentSteeringTorque = steeringTorque * Input.GetAxis("Horizontal");
+
+        FrontLeftWheelCollider.steerAngle = presentSteeringTorque;
+        FrontRightWheelCollider.steerAngle = presentSteeringTorque;
     }
 }
